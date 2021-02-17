5b14ae7460f043620473e533816c95c5c83397e1
Mizeloble <Mizeloble@gmail.com>
2021-02-17 17:26:37 +0900

    Landing Page Implementation

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,7 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+//The Movie DB API
+export const API_KEY = 'c39fd03a62eecdedb96285a58d8c3a7a';
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
